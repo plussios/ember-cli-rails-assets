61be41f36337fb2d9fe07d61c04c4ead2981964f
Stan Sidel <stan@plussios.com>
2021-04-13 11:54:47 +0500

    PA-174 Fixed a deprecation warning

--- lib/ember_cli/assets/engine.rb
+++ lib/ember_cli/assets/engine.rb
@@ -1,8 +1,10 @@
-module EmberCli
-  module Assets
-    class Engine < ::Rails::Engine
-      initializer "ember-cli-rails-assets" do
-        ActionController::Base.helper EmberCliRailsAssetsHelper
+ActiveSupport.on_load(:action_controller) do
+  module EmberCli
+    module Assets
+      class Engine < ::Rails::Engine
+        initializer "ember-cli-rails-assets" do
+          ActionController::Base.helper EmberCliRailsAssetsHelper
+        end
       end
     end
   end
